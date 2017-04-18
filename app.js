5273db4fadcc4800c7ce8c882940cc3a0c9ea1ab
eshengsky <eshengsky@163.com>
2017-04-18 20:06:11 +0800

    Jade->Pug

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
